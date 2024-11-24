20c4e7e9fe6ba0573f8b9e601c12c29042ccd11b
lkhaajav <lkhaching@gmail.com>
2024-11-24 20:43:15 +0900

    init code

---
+++ src/main/java/nuudelchin/club/web/SessionOauthApplication.java
@@ -0,0 +1,13 @@
+package nuudelchin.club.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nuudelchin/club/web/SessionOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package nuudelchin.club.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
